2739f621b3b5637b26839952ae74339437617843
Ruhshan <ruhshan.ahmed@gmail.com>
2023-04-07 09:47:48 +0600

    Hide form helper when error message is displayed

--- frontend/src/components/Analyzer.tsx
+++ frontend/src/components/Analyzer.tsx
@@ -1,21 +1,52 @@
-import {Container, Textarea, VStack, FormControl, FormLabel, FormHelperText} from '@chakra-ui/react';
+import {
+    Container,
+    Textarea,
+    VStack,
+    FormControl,
+    FormLabel,
+    FormHelperText,
+    FormErrorMessage,
+    Button, Spacer
+} from '@chakra-ui/react';
 import React from 'react';
 
 export function Analyzer() {
-    const [seq, setSeq] = React.useState('')
+    const [seq, setSeq] = React.useState('');
+    const [invalidInput, setInvalidInput] = React.useState(false);
 
-    const handleInputChange = (e)=>{
-        console.log(e.target.value)
+    const handleInputChange = (e) => {
+        console.log(isSequenceValid(e.target.value));
+        if (isSequenceValid(e.target.value)) {
+            setInvalidInput(false);
+        } else {
+            setInvalidInput(true);
+        }
+    };
+
+    function isSequenceValid(sequence: string): boolean {
+        const regex = /^[ACDEFGHIKLMNPQRSTVWY]+$/i;
+        return regex.test(sequence);
     }
 
     return (
         <VStack>
-            <Container color='black'>
-                <FormControl>
+            <Container color="black">
+                <FormControl isInvalid={invalidInput}>
                     <FormLabel>Enter Protein Sequence:</FormLabel>
                     <Textarea onChange={handleInputChange}></Textarea>
-                    <FormHelperText>Currently we don't support FASTA</FormHelperText>
+
+
+                    {invalidInput ? (<FormErrorMessage>Invalid input</FormErrorMessage>) :
+                        (<FormHelperText>Currently we don&apos;t support FASTA</FormHelperText>)
+                    }
+
                 </FormControl>
+
+                <div style={{'marginTop':'10px'}}>
+                    <Button colorScheme='teal' variant='outline' size='sm'>
+                        Search
+                    </Button>
+                </div>
             </Container>
         </VStack>
     );
