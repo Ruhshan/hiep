adf8353fff2133dc444a35890986a3303c876a61
Ruhshan <ruhshan.ahmed@gmail.com>
2023-04-13 11:28:45 +0600

    Enable handling validation error

--- backend/api/errorHandler.go
+++ backend/api/errorHandler.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/Ruhshan/hiep/backend/pkg/errorMessages"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"net/http"
+	"strings"
 )
 
 type ErrorResponse struct {
@@ -31,6 +33,9 @@ func ErrorHandler(c *gin.Context) {
 
 	if lastError!=nil{
 		switch lastError.Err.(type) {
+			case validator.ValidationErrors:
+				apiError = getMessageForValidationError(lastError.Err.(validator.ValidationErrors))
+				break
 			case *json.UnmarshalTypeError:
 				status = http.StatusBadRequest
 				apiError.Error = "Invalid value at: "+lastError.Err.(*json.UnmarshalTypeError).Field
@@ -46,10 +51,29 @@ func ErrorHandler(c *gin.Context) {
 
 }
 
+func getMessageForValidationError(errs validator.ValidationErrors) ErrorResponse {
+
+	var field = toCamelCase(errs[0].Field())
+	var tag = errs[0].Tag()
+
+	return ErrorResponse{fmt.Sprintf("Validation failed on field: %s, error: %s", field, tag)}
+}
+
+func toCamelCase(s string) string {
+	if s == "" {
+		return ""
+	}
+	lower := strings.ToLower(s)
+	return strings.Replace(lower[:1]+s[1:], s[:1], lower[:1], 1)
+}
+
 func getMessageForError(e error)  ErrorResponse{
+
 	switch e {
 	case errorMessages.ErrContainsInvalidCharacters:
-		return ErrorResponse{"Input Contains invalid character"}
+		return ErrorResponse{"Input contains invalid character"}
+	case errorMessages.ErrContainsMoreThanOneFastaSequence:
+		return ErrorResponse{"Input contains more than one fasta"}
 	default:
 		return ErrorResponse{"Unknown Error"}
 	}
